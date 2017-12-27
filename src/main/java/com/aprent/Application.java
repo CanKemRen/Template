6c9626599b9fb3498d5e623e6ffab8f59dc15810
mustafa.kemal.oecel <oecel@pwersonalmarkt.de>
2017-12-27 11:23:00 +0100

    repair messagebundle system

--- src/main/java/com/aprent/Application.java
+++ src/main/java/com/aprent/Application.java
@@ -1,24 +1,31 @@
 package com.aprent;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import org.thymeleaf.spring4.SpringTemplateEngine;
 
-import java.util.Locale;
-
+@Configuration
+@EnableWebMvc
+@ComponentScan
 @SpringBootApplication
 @EnableMongoRepositories
-public class Application  extends WebMvcConfigurerAdapter {
+public class Application extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -27,33 +34,52 @@ public class Application  extends WebMvcConfigurerAdapter {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler(
-				"/webjars/**",
-				"/img/**",
-				"/css/**",
-				"/js/**")
-				.addResourceLocations(
-						"classpath:/META-INF/resources/webjars/",
-						"classpath:/static/img/",
-						"classpath:/static/css/",
-						"classpath:/static/js/");
+			"/webjars/**",
+			"/img/**",
+			"/css/**",
+			"/js/**")
+			.addResourceLocations(
+				"classpath:/META-INF/resources/webjars/",
+				"classpath:/static/img/",
+				"classpath:/static/css/",
+				"classpath:/static/js/");
 	}
 
 	@Bean
 	public LocaleResolver localeResolver() {
-		SessionLocaleResolver slr = new SessionLocaleResolver();
-		return slr;
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.GERMAN);
+		return localeResolver;
 	}
 
 	@Bean
 	public LocaleChangeInterceptor localeChangeInterceptor() {
-		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-		lci.setParamName("lang");
-		lci.setParamName("locale");
-		return lci;
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		return localeChangeInterceptor;
 	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(localeChangeInterceptor());
 	}
+
+	@Bean
+	public MessageSource emailSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages/emails");
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setFallbackToSystemLocale(false);
+		return messageSource;
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("lang/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+
 }
